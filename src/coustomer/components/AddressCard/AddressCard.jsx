fbde92bc729f007555925276893b27c3cef5581c
LazY <darshitmistry2@gmail.com>
2024-03-17 23:34:24 +0530

    style : checkout and delevered address page

---
+++ src/coustomer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AddressCard = () => {
+  return (
+    <div>
+        <div className='space-y-3 px-5 py-5 rounded-md border border-gray-300'>
+            <p className='font-semibold'>Kachra Sheth</p>
+            <p>Bagla number 2 ,Shaitan gali,mumbai,00456</p>
+            <div className='space-y-1'>
+                <p className='font-semibold'>Phone Number </p>
+                <p>Samsung Ka Number</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddressCard
