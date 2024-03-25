29e774557e85a77154b116a891a8ee11c7dfbf2a
LazY <darshitmistry2@gmail.com>
2024-03-25 09:19:42 +0530

    feat : admin routers setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import CustomerRoutes from "./Routers/CustomerRoutes";
+import AdminRouters from "./Routers/AdminRouters";
 
 const App = () => {
   return (
     <div className="">
       <Routes>
         <Route path="/*" element={<CustomerRoutes/>}/>
+        <Route path="/admin/*" element={<AdminRouters/>}/>
       </Routes>
     </div>
   );
